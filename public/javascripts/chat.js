e05ee1d0b7201e12303b9521e2685fa36fc4072b
YaegerKnight <kbnaveen20@gmail.com>
2018-04-14 12:13:35 +0000

    Material Design applied  14/04/2018 17:45

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,42 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
+    $scope.messages = [
+        {
 
+            'sender': 'USER',
+            'text': 'Hello'
+
+		},
+        {
+
+            'sender': 'BOT',
+            'text': 'Hello USER'
+
+		},
+        {
+
+            'sender': 'USER',
+            'text': 'What are you?'
+
+		},
+        {
+
+            'sender': 'BOT',
+            'text': 'ggwp'
+
+		},
+        {
+
+            'sender': 'USER',
+            'text': 'Tell a joke.'
+
+		},
+        {
+
+            'sender': 'BOT',
+            'text': 'What did one ocean say to the other? Nothing.They just waved.'
+
+		}
+ 	];
 });
