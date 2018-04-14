d0e35e212058567da44c65c5eb19fc2c437c4644
YaegerKnight <kbnaveen20@gmail.com>
2018-04-14 07:24:44 +0000

    Added getFeedByQuery 12:56 14/04/2018

--- app/services/FeedService.java
+++ app/services/FeedService.java
@@ -11,5 +11,26 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class FeedService {
+    public FeedResponse getFeedByQuery(String query){
+        FeedResponse feedResponse = new FeedResponse();
+        try{
+            WSRequest queryRequest = WS.url("https://news.google.com/news");
+            CompletionStage<WSResponse> responsePromise = queryRequest
+                    .setQueryParameter("query",query)
+                    .setQueryParameter("output","rss")
+                    .get();
+
+            Document response = responsePromise.thenApply(WSResponse::asXml).toCompletableFuture().get();
+            Node item = response.getFirstChild().getChildNodes().item(10);
+            feedResponse.title = item.getChildNodes().item(0).getFirstChild().getNodeValue();
+           // feedResponse.pubDate = item.getFirstChild().getChildNodes().item(4).toString();
+           // feedResponse.description = item.getChildNodes().item(5).toString();
+            feedResponse.pubDate = item.getChildNodes().item(4).getFirstChild().getNodeValue();
+            feedResponse.description = item.getChildNodes().item(5).getFirstChild().getNodeValue();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return feedResponse;
+    }
 
 }
