aa6be1bfb9cbcac25c5aee2cd2c9c8c353e341ff
YaegerKnight <kbnaveen20@gmail.com>
2018-04-14 06:05:13 +0000

    Added newsAgentService 14/04/2018 11:35

--- app/data/NewsAgentResponse.java
+++ app/data/NewsAgentResponse.java
@@ -1,5 +1,9 @@
 package data;
+import java.io.*;
 
 public class NewsAgentResponse {
+    public String query = new String();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 
 }
